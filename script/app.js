380c7e5689f3b3c4ff550d04fcc2b079848e054a
eduvinagre <eduvinagre@gmail.com>
2023-04-15 19:48:34 -0300

    added item total and cart total

--- script/app.js
+++ script/app.js
@@ -85,9 +85,23 @@ displayProducts(products) {
 
                     Storage.saveCart(cart)
 
+                    // set cart values
+
+                    this.setCartValues(cart)
+
                 })
             
         })
+    }
+    setCartValues(cart) {
+        let tempTotal = 0
+        let itemsTotal = 0
+        cart.map(item => {
+            tempTotal += item.price * item.amount
+            itemsTotal += item.amount
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+        cartItems.innerText = itemsTotal
     }
 }
 
