b21bab2b087196f96d3012bde35ca7e7d74eaf0b
eduvinagre <eduvinagre@gmail.com>
2023-04-16 00:18:39 -0300

    added clean cart function

--- script/app.js
+++ script/app.js
@@ -143,6 +143,30 @@ displayProducts(products) {
         cartOverlay.classList.remove("transparentBcg")
         cartDOM.classList.remove("showCart")
     }
+    cartLogic() {
+        clearCartBtn.addEventListener('click', () => {
+            this.clearCart()
+        })
+    }
+    clearCart() {
+        let cartItems = cart.map(item => item.id)
+        cartItems.forEach(id => this.removeItem(id));
+        while(cartContent.children.length > 0) {
+            cartContent.removeChild(cartContent.children[0])
+        }
+        this.hideCart()
+    }
+    removeItem(id) {
+        cart = cart.filter(item => item.id !== id)
+        this.setCartValues(cart)
+        Storage.saveCart(cart)
+        let button = this.getSingleButton(id)
+        button.disabled = false
+        button.innerHTML = `<i class="fas fa-shopping-cart"></i>adicionar`
+    }
+    getSingleButton(id) {
+        return buttonsDOM.find(button => button.dataset.id === id)
+    }
 }
 
 //local storage
@@ -174,5 +198,6 @@ ui.setupApp()
         Storage.saveProducts(products)
     }).then(() => {
         ui.getBagButtons()
+        ui.cartLogic()
     })
 })
